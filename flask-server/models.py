af310dc0964fb635593b852362b6484d108e538d
c0ffee <platinumspoons@gmail.com>
2022-04-02 18:10:03 -0700

    added api for user registry

--- flask-server/models.py
+++ flask-server/models.py
@@ -2,13 +2,19 @@ from server import db
 from werkzeug.security import check_password_hash, generate_password_hash
 
 class User(db.Model):
-    def __init__(self, username, password):
-        self.username = username
+    def __init__(self, email, password, firstName, lastName):
+        self.email = email
         self.set_password(password)
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
+
+    email = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+
+    firstName = db.Column(db.String(64), index=True)
+    lastName = db.Column(db.String(64, index=True))
+
+
 
 
     def set_password(self, password):
--- flask-server/routes.py
+++ flask-server/routes.py
@@ -1,7 +1,19 @@
-from server import app
+from server import app, db
+from flask import request
+from models import *
 
-from models import User
-
-@app.route("/")
+@app.route("/members")
 def members():
     return {"members": ["Member1", "Member2", "Member3"]}
+
+@app.route("/register_user", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        userRegisterData = request.form.to_dict()
+        user = User(userRegisterData["email"], userRegisterData["password"], userRegisterData["firstName"], userRegisterData["lastName"])
+
+        if user is not None:
+            db.session.add(user)
+            db.session.commit()
+
+
