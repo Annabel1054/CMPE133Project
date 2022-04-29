e060c69c5c1ca2ca0adf8498bb0915648140cb08
PhannJimmy <phannnjimmy@gmail.com>
2022-04-28 23:00:00 -0700

    Login Page

--- frontend/src/Components/Login/Login.js
+++ frontend/src/Components/Login/Login.js
@@ -1,11 +1,48 @@
-import NavBar
-    from "../Navbar/Navbar";
-export default function LoginPage() {
+import NavBar from "../Navbar/Navbar";
+import { useState } from 'react';
+import { Button, Form} from 'react-bootstrap';
+import './Login.css';
 
-    return (
-        <div>
-            <NavBar />
-            LOGIN PAGE
+export default function LoginPage(){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log("Email: " + email);
+        console.log("Password: " + password);
+    }
+    return (   
+        <div class = "background">
+             <NavBar/>
+            <div class = "loginbox">
+                Login
+            <Form className='formContainer' onSubmit={onSubmit}>
+                    <Form.Group>
+                        <Form.Label >
+                            Account E-mail: 
+                        </Form.Label>
+                        <Form.Control
+                            input type='text'
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                            placeholder="Enter E-mail"
+                        />
+                    </Form.Group>
+                    <Form.Group className= "input">
+                        <Form.Label>
+                            Password: 
+                        </Form.Label>
+                        <Form.Control
+                            input type='password'
+                            placeholder="Enter Password"
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                        />
+                    </Form.Group>
+                <Button type="login_button">Login</Button>
+            </Form>
+            </div>
         </div >
     );
 };
