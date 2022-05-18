2ec8c342d81e02d5b1eb0d2488ea2f2a65c3e976
c0ffee <platinumspoons@gmail.com>
2022-05-18 12:38:16 -0700

    buyers interested

--- flask-server/routes.py
+++ flask-server/routes.py
@@ -187,7 +187,22 @@ def textbook_array_to_json(textbooks):
                 "\"courseName\": \"" + t.courseName + "\"," + \
                 "\"description\": \"" + t.description + "\"," + \
                 "\"quality\": \"" + t.quality + \
-                "\"},"
+                "\"buyers\": ["
+                
+            for b in t.users:
+                jsonTextbooks = jsonTextbooks + "{" + \
+                "\"buyerFirstName\": \"" + b.firstName + "\"," + \ 
+                "\"buyerLastName\": \"" + b.lastName + "\"," + \ 
+                "\"buyerEmail\": \"" + b.email + "\"," + \ 
+                "\"buyerPhoneNum\": \"" + b.phoneNum + "\"}"
+
+                if b == textbooks[-1]:
+                    jsonTextbooks = jsonTextbooks + ","
+                else
+                    jsonTextbooks = jsonTextbooks + "]"
+                
+
+            jsonTextbooks = jsonTextbooks + "\"},"
         else:
             jsonTextbooks = jsonTextbooks + "{" + \
                 "\"id\": \"" + str(t.id) + "\"," + \
@@ -203,7 +218,21 @@ def textbook_array_to_json(textbooks):
                 "\"courseName\": \"" + t.courseName + "\"," + \
                 "\"description\": \"" + t.description + "\"," + \
                 "\"quality\": \"" + t.quality + \
-                "\"}"
+                "\"buyers\": ["
+                
+            for b in t.users:
+                jsonTextbooks = jsonTextbooks + "{" + \
+                "\"buyerFirstName\": \"" + b.firstName + "\"," + \ 
+                "\"buyerLastName\": \"" + b.lastName + "\"," + \ 
+                "\"buyerEmail\": \"" + b.email + "\"," + \ 
+                "\"buyerPhoneNum\": \"" + b.phoneNum + "\"}"
+
+                if b == textbooks[-1]:
+                    jsonTextbooks = jsonTextbooks + ","
+                else
+                    jsonTextbooks = jsonTextbooks + "]"
+
+            jsonTextbooks = jsonTextbooks + "\"}"
 
     jsonTextbooks = jsonTextbooks + "]}"
 
