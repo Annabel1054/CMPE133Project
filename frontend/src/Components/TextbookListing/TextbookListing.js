c5b3c9c505ca8ef85ffde7e2496f4a40bf8908a4
Annabel1054 <annabelkusumo@gmail.com>
2022-05-18 11:12:39 -0700

    Setup watchlist page, added api call for add to watchlist

--- frontend/src/Components/TextbookListing/TextbookListing.js
+++ frontend/src/Components/TextbookListing/TextbookListing.js
@@ -3,7 +3,34 @@ import calculusImage from "./Calculus Textbook.jpg"
 import './styles.css';
 
 export default function TextbookListing(props) {
-    const { title, isbn, author, price, quality, description, course, name, originalPrice } = props;
+    const email = localStorage.getItem('email');
+    const { title, isbn, author, price, quality, description, course, name, originalPrice, id } = props;
+
+    const addToWatchlist = () => {
+
+        let watchlistData = {
+            email: email,
+            id: id,
+        }
+
+        fetch("http://127.0.0.1:5000/modify_listing", {
+            method: 'POST',
+            body: JSON.stringify(watchlistData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(data => {
+                if (data.status !== 200)
+                    alert("Having error")
+                else {
+                    alert("Successfully updated your textbook listing.")
+                }
+            })
+            .catch(function (error) {
+                console.log("Fetch error: " + error);
+            });
+    }
 
     return (
         <div className="listingContainer">
@@ -24,7 +51,7 @@ export default function TextbookListing(props) {
                     <div>
                         <Card.Text className="oldPrice">Original ${originalPrice}</Card.Text>
                         <Card.Text className="price">${price}</Card.Text>
-                        <Button className="addToWatchlist" style={{ width: '150px', backgroundColor: '#829A7E' }} variant="primary">Add to Watchlist</Button>
+                        <Button className="addToWatchlist" onClick={addToWatchlist} style={{ width: '150px', backgroundColor: '#829A7E' }} variant="primary">Add to Watchlist</Button>
                     </div>
 
                 </Card.Body>
--- frontend/src/Components/Watchlist/watchlist.js
+++ frontend/src/Components/Watchlist/watchlist.js
@@ -3,9 +3,41 @@ import { Container, Button, Card, Text } from "react-bootstrap";
 import calculusImage from "./Calculus Textbook.jpg";
 import "./WatchlistStyles.css";
 import WatchlistListing from "./WatchlistListing";
+import React, { useEffect, useState } from 'react';
 
 
 export default function WatchList() {
+    const [watchlistListings, setWatclistListings] = useState([]);
+    const email = localStorage.getItem('email');
+
+    useEffect(() => {
+        if (email !== undefined) {
+            let userEmail = {
+                email: email
+            }
+
+            fetch("http://127.0.0.1:5000/manage_listings", {
+                method: 'POST',
+                body: JSON.stringify(userEmail),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(data => {
+                    if (data.status !== 200)
+                        alert("Having error")
+                    else {
+                        return data.json();
+                    }
+                }).then(data => {
+                    setWatclistListings(data);
+                })
+                .catch(function (error) {
+                    console.log("Fetch error: " + error);
+                });
+        }
+    }, [])
+
 
     return (
         <div className="maindiv">
@@ -13,18 +45,26 @@ export default function WatchList() {
 
             <div className="watchlistContainer">
                 <div className="watchlistHeader">Your Watchlist</div>
-                <WatchlistListing
-                    title="Calculus"
-                    author="Larson Hostetler Edwards"
-                    isbn="9809238423"
-                    quality="Good"
-                    course="MATH31"
-                    description="This textbook is new."
-                    oldPrice="100"
-                    price="87"
-                    name="John Doe"
-                />
+                {(typeof watchlistListings.textbooks === 'undefined') ? (
+                    <p> No Textbooks In Watchlist! Start Searching :) </p>
+                ) : (
+                    watchlistListings.textbooks.map((listing) => (
+                        <WatchlistListing
+                            title={listing.title}
+                            author={listing.author}
+                            isbn={listing.isbn}
+                            quality={listing.quality}
+                            course={listing.courseName}
+                            description={listing.description}
+                            price={listing.price}
+                            oldPrice={listing.originalPrice}
+                            id={listing.id}
+                            email={listing.email}
+                        />
+                    ))
+                )}
             </div>
+
 
         </div>
 
